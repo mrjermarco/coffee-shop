aa03b3291ba1b24affe946d2fff437e36cb5fbbc
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-04-24 20:37:22 -0500

    first commit

---
+++ coffee-shop-backend/src/main/java/org/launchcode/coffeeshopbackend/CoffeeShopBackendApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.coffeeshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopBackendApplication.class, args);
+	}
+
+}
---
+++ coffee-shop-backend/src/test/java/org/launchcode/coffeeshopbackend/CoffeeShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.coffeeshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
