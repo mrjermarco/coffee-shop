2cbd9bcc03c23dda7e63a3f1c41f9c43e6171993
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-04-27 20:38:27 -0500

    Added register-user component

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterUserPageComponent
   ],
   imports: [
     BrowserModule
