b25e2bb4d05c18075876343097721b6b5c2bb048
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-04-28 23:22:16 -0500

    Added routing for register user page

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes:Routes=[{
+  path:'register', component:RegisterUserPageComponent
+}]
 @NgModule({
   declarations: [
     AppComponent,
     RegisterUserPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
