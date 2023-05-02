e65348a69c0c8c712b9be6ea9be5996250e1deea
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-05-01 21:56:19 -0500

    re-added lost components

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
@@ -29,5 +29,6 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
 
