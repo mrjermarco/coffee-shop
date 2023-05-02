06c9ebd764774774dd2ce11c12a3bfa3fe4b9f34
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-05-01 20:50:43 -0500

    admin component merge

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'user-login', component: UserLoginPageComponent },
-  { path: 'register-user', component: RegisterUserPageComponent }
+  { path: 'register-user', component: RegisterUserPageComponent },
+  { path: 'admin-login', component: AdminLoginComponent }
 ];
 
 
@@ -17,7 +19,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     UserLoginPageComponent,
-    RegisterUserPageComponent
+    RegisterUserPageComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
